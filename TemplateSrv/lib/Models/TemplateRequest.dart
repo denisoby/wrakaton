b5f6ad4ab52425301680b8e077f8e7a2711b5587
Vladislav Gordievskiy <vladislav.gordievskiy@team.wrike.com>
2016-07-16 19:11:38 +0300

    init template request

---
+++ TemplateSrv/lib/Models/TemplateRequest.dart
@@ -0,0 +1,20 @@
+library template_srv.models.task_template;
+import 'package:embla_trestle/embla_trestle.dart';
+
+class TemplateRequest extends Model {
+  @field int id;
+  @field int user_id;
+  @field int base_template_id;
+  @field List nested_templates;
+  @field Map data;
+
+  Map toJson() {
+    return {
+      'id' : id,
+      'user_id' : user_id,
+      'base_template_id' : base_template_id,
+      'nested_templates' : nested_templates,
+      'data' : data
+    };
+  }
+}
