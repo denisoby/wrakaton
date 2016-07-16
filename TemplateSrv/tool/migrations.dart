2a1eb69f297cd4e0d84c05b5feec877fb1f03956
Vladislav Gordievskiy <vladislav.gordievskiy@team.wrike.com>
2016-07-16 19:13:39 +0300

    update migrations

--- TemplateSrv/tool/migrations.dart
+++ TemplateSrv/tool/migrations.dart
@@ -4,7 +4,8 @@ import 'package:srv_base/Tools/migrations.dart';
 
 final migrations = [
   CreateUsersTableMigration,
-  CreateTemplatesTableMigration
+  CreateTemplatesTableMigration,
+  CreateTemplateRequestsTableMigration
 ].toSet();
 
 class CreateTemplatesTableMigration extends Migration {
@@ -21,6 +22,29 @@ class CreateTemplatesTableMigration extends Migration {
       schema.int('type').nullable(false);
       schema.json('data');
       schema.json('nested');
+    });
+  }
+
+  @override
+  Future rollback(Gateway gateway) async {
+    await gateway.drop(table_name);
+  }
+}
+
+class CreateTemplateRequestsTableMigration extends Migration {
+
+  String table_name = 'template_requests';
+
+  @override
+  Future run(Gateway gateway) async {
+    await gateway.create(table_name, (schema) {
+      schema.id();
+      schema.timestamp('created_at').nullable(false);
+      schema.timestamp('updated_at').nullable(false);
+      schema.int('user_id').nullable(false);
+      schema.int('base_template_id').nullable(false);
+      schema.json('nested_templates');
+      schema.json('data');
     });
   }
 
