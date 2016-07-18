4a010dfd6c40fec7e573c5738ea14bb4ecc66eed
Denis Obydennykh <denis.obydennykh@team.wrike.com>
2016-07-19 00:46:07 +0300

    templateclient test

--- TemplateClient/lib/TemplateClient.dart
+++ TemplateClient/lib/TemplateClient.dart
@@ -1,11 +1,6 @@
 // Copyright (c) 2016, Denis Obydennykh. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library TemplateClient;
 
 export 'src/TemplateClient_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
--- TemplateClient/lib/src/TemplateClient_base.dart
+++ TemplateClient/lib/src/TemplateClient_base.dart
@@ -1,9 +1,8 @@
 // Copyright (c) 2016, Denis Obydennykh. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-// TODO: Put public facing types in this file.
-
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+class TemplateClient {
+  void test(){
+    print("TemplateClient Ehhlo, lorg!");
+  }
 }
