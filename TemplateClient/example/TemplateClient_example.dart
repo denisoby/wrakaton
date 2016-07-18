9137233481e64e9444d7c6c9413f26eb2a7f2765
Denis Obydennykh <denis.obydennykh@team.wrike.com>
2016-07-19 00:30:46 +0300

    init v2 with stagehand

---
+++ TemplateClient/example/TemplateClient_example.dart
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, Denis Obydennykh. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:TemplateClient/TemplateClient.dart';
+
+main() {
+  var awesome = new Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
